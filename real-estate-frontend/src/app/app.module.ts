a51ac6f91257c8dd9d42ab884a888d65a0470768
praveen-neshvi <71161341+praveen-neshvi@users.noreply.github.com>
2023-02-23 21:19:44 +0530

    Template Form used to update Add new property component

--- real-estate-frontend/src/app/app.module.ts
+++ real-estate-frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -10,6 +11,7 @@ import { NavbarComponent } from './property/navbar/navbar.component';
 import { HousingService } from './services/housing-service.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+
 // const appRoutes: Routes;
 
 @NgModule({
@@ -24,6 +26,7 @@ import { PropertyDetailComponent } from './property/property-detail/property-det
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(
       [
         { path: '', component: PropertyListComponent },
--- real-estate-frontend/src/app/property/add-property/add-property.component.ts
+++ real-estate-frontend/src/app/property/add-property/add-property.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +9,9 @@ import { Router } from '@angular/router';
 })
 export class AddPropertyComponent implements OnInit{
 
+  @ViewChild('FormVariable')
+  appPropertyFormVariable!: NgForm;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -16,6 +20,10 @@ export class AddPropertyComponent implements OnInit{
 
   onBack(){
     this.router.navigate(['/']);
+  }
+
+  onSubmit(){
+    console.log(this.appPropertyFormVariable);
   }
 
 }
